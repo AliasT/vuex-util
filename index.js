6492365643f21722f1855de6ba4dce3c1b910ea3
xiaobing <1481569358@qq.com>
2017-07-16 01:13:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,33 @@ export function factoryObject(field) {
   }
 }
 
+
+/**
+ * 动态生成vuex async action(结合服务器api设计更改
+ * @param {string} requestUrlTemplate 请求字符串 "/api/albums/:id/course/:courseId"
+ * @param {string} method 请求类型
+ *
+ *
+ * usage: [types.GET_USER_POSTS_SUCESS]: asyncAction('/api/user/:userId/posts/:postId/', types.GET_USER_POSTS_SUCESS)
+ */
+export function asyncAction (requestUrlTemplate, COMMIT_TYPE, _method='get',) {
+  let method = get
+  if (method == 'put') method = put
+  else if (method == 'post') method = post
+  
+  /**
+   * redux async action builder
+   * 😘😘 🎉🎉 😏😏
+   */
+  return async function ({ commit, rootState, state }, payload) {
+    const url = requestUrlTemplate.replace(/(:(.+?)(\/|$))/g, function(match, p1, p2, p3) {
+      const ret = [payload[p2], p3].join('')
+      delete payload[p2]
+      return ret
+    })
+    /* http request */
+    const json = await method(url, { data: payload })
+    commit(COMMIT_TYPE, json)
+    return json 
+  }
+}
